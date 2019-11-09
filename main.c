3eb02e2e02ab44ed5e970320e0665dcbcc6b6477
renatoabreu14 <renato.abreu.info@gmail.com>
2019-11-09 15:56:03 -0300

    Aula dia 08/11/2019

--- main.c
+++ main.c
@@ -59,8 +59,109 @@ void removeFim(struct listaEnc *pList){
     }
 }
 
+int numeroDeElementos(struct listaEnc pList){
+    int cont = 0;
+    struct no *p;
+    for(p = pList.prim; p != NULL; p = p->prox){
+        cont++;
+    }
+    return cont;
+}
+
+int posicaoDoElemento(struct listaEnc pList, int valor){
+    int cont = 0;
+    struct no *p;
+    for(p = pList.prim; p != NULL; p = p->prox){
+        if (p->valor == valor){
+            return cont;
+        }
+        cont++;
+    }
+    return -1;
+}
+
+void insereAposValor(struct listaEnc *pList, int valor, int inserir){
+    struct no *p;
+    for(p = pList->prim; p != NULL; p = p->prox){
+        if (p->valor == inserir){
+            struct no *novo;
+            novo = (struct no*)malloc(sizeof(struct no));
+            novo->valor = valor;
+            novo->prox = p->prox;
+            p->prox = novo;
+        }
+    }
+}
+
+void insereAntesValor(struct listaEnc *pList, int valor, int inserir){
+    struct no *p;
+    struct no *pAux;
+    for(p = pList->prim; p->prox != NULL; p = p->prox){
+        pAux = p->prox;
+        if (pAux->valor == inserir){
+            struct no *novo;
+            novo = (struct no*)malloc(sizeof(struct no));
+            novo->valor = valor;
+            novo->prox = pAux;
+            p->prox = novo;
+            p = p->prox;
+        }
+    }
+}
+
+void inverterLista(struct listaEnc *pList){
+    struct no *p, *q, *r;
+    if (pList->prim != NULL){
+        if ((pList->prim)->prox != NULL){
+            r = NULL;
+            q=pList->prim;
+            for (p=(pList->prim)->prox; p!= NULL; p=p->prox){
+                q->prox = r;
+                r=q;
+                q=p;
+            }
+            q->prox = r;
+            pList->prim = q;
+        }
+    }
+}
+
 int listaVazia(struct listaEnc pList){
     return (pList.prim == NULL);
+}
+
+void insereOrd(struct listaEnc *pList, int valor){
+    struct no *novo, *p, *q;
+    novo = (struct no*)malloc(sizeof(struct no));
+    novo->valor = valor;
+    novo->prox = NULL;
+
+    if (pList->prim == NULL){
+        pList->prim = novo;
+    }
+    else{	//Posicionando o ponteiro "p" na frente e o ponteiro "q" no elemento anterior (o novo elemento será inserido entre p e q)
+        q = NULL;
+        for (p=pList->prim; p!=NULL; p=p->prox){
+            if (p->valor > valor)
+                break;
+            q = p;
+        }
+        if (q == NULL){
+            // O novo elemento deve ser inserido no início da lista
+            novo->prox = pList->prim;
+            pList->prim = novo;
+            return;
+        }
+        if (p == NULL){
+            // O novo elemento deve ser inserido no fim da lista (após o ponteiro "q")
+            q->prox = novo;
+            return;
+        }
+        // Se o fluxo de execução chegou até aqui... então o elemento deve ser inserido entre p e q
+        novo->prox = p;
+        q->prox = novo;
+    }
+
 }
 
 int main() {
@@ -97,6 +198,9 @@ int main() {
                 break;
             }
             case 3:{
+                printf("Informe o valor:");
+                scanf("%d", &valor);
+                insereOrd(&minhaLista, valor);
                 break;
             }
             case 4:{
@@ -119,15 +223,51 @@ int main() {
                 break;
             }
             case 8:{
-                printf("A lista contem $d elemento(s)", numerodeelementos(minhaLista));
+                printf("A lista contem %d elemento(s)\n", numeroDeElementos(minhaLista));
+                break;
+            }
+            case 9:{
+                printf("Informe o valor:");
+                scanf("%d", &valor);
+                int posicao = posicaoDoElemento(minhaLista, valor);
+                if (posicao == -1){
+                    printf("Valor não encontrado");
+                }else{
+                    printf("O elemento de valor %d esta na posicao %d\n", valor, posicao);
+                }
+                break;
+            }
+            case 10:{
+                int inserir;
+                printf("Informe o valor a ser inserido:");
+                scanf("%d", &valor);
+                printf("Deseja inserir apos o valor:");
+                scanf("%d", &inserir);
+                insereAposValor(&minhaLista, valor, inserir);
+                break;
+            }
+            case 11:{
+                int inserir;
+                printf("Informe o valor a ser inserido:");
+                scanf("%d", &valor);
+                printf("Deseja inserir antes o valor:");
+                scanf("%d", &inserir);
+                insereAntesValor(&minhaLista, valor, inserir);
+                break;
+            }
+            case 12:{
+                inverterLista(&minhaLista);
+                printf("A lista foi invertida\n");
+                mostrarLista(minhaLista);
                 break;
             }
         }
+
     }while(op != 13);
 }
 
 //numero de elementos
 //em qual posição o elemento se encontra sabendo que a primeira posição eh 0
-//inserir após valor
-//inserir antes valor
+//inserir após um determinado valor
+//inserir antes um determinado valor
 //inverter lista
